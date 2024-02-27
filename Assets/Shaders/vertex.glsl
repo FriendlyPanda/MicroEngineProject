24a06d944034a0878bb91b9b259f4e644b19b9e1
JanFH <jan.halaczek@live.com>
2024-02-27 01:29:08 +0000

    slight change to vertex shader

--- Assets/Shaders/vertex.glsl
+++ Assets/Shaders/vertex.glsl
@@ -7,6 +7,6 @@ out vec3 color;
 uniform float scale;
 
 void main(){
-    gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+    gl_Position = vec4(aPos.x + (aPos.x * scale), aPos.y + (aPos.y * scale), aPos.z, 1.0);
     color = aColor;
 }
