822b1bf678db8f24b6124f6a6a0a863f514ba80d
JanFH <jan.halaczek@live.com>
2024-04-23 13:06:05 +0100

    added ribbon generation

--- src/Game/Graphics/GE.cpp
+++ src/Game/Graphics/GE.cpp
@@ -37,6 +37,8 @@ int GraphicsEngine::_run() {
     glfwSwapInterval(1);
 
     glEnable(GL_DEPTH_TEST);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     rbo.RBO_create(windowWidth, windowHeight);
     txt.texture_create(windowWidth, windowHeight);
